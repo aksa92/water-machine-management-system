85fe0bd79bc8fa448a7f402e10344f1e46628848
周竞由 <1193626695@qq.com>
2025-12-02 21:12:13 +0800

    注册接口

---
+++ src/main/java/com/campus/water/controller/RegisterController.java
@@ -0,0 +1,35 @@
+package com.campus.water.controller;
+
+import com.campus.water.entity.dto.request.RegisterRequest;
+import com.campus.water.service.RegisterService;
+import com.campus.water.util.ResultVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+/**
+ * 注册接口控制器（公共接口）
+ */
+@RestController
+@RequestMapping("/api/common")
+public class RegisterController {
+
+    @Autowired
+    private RegisterService registerService;
+
+    /**
+     * 用户注册接口
+     * @param registerRequest 注册请求参数
+     * @return 注册结果
+     */
+    @PostMapping("/register")
+    public ResponseEntity<ResultVO<Boolean>> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        boolean success = registerService.register(registerRequest);
+        return ResponseEntity.ok(ResultVO.success(success, "注册成功"));
+    }
+}
